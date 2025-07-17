b70e4bf71701c6a26856fcb128f47f20076aa397
Davi Simoes Dos Santos <dasimoes@c1r8p4.42sp.org.br>
2025-07-16 22:47:18 -0300

    first core structure version

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: dasimoes <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/14 12:59:00 by dasimoes          #+#    #+#             */
-/*   Updated: 2025/07/15 13:28:36 by dasimoes         ###   ########.fr       */
+/*   Updated: 2025/07/16 22:45:18 by dasimoes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,41 @@
 
 int	ft_printf(const char *format, ...)
 {
-	va_list list;
+	va_list	list;
+	int		count;
+	int		i;
 
+	i = 0;
+	count = 0;
 	va_start(list, format);
-	
-	va_arg(list, type);
-
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 'c')
+				count += ft_putchar(va_arg(list, int));
+			if (format[i] == 's')
+				count += ft_putstr(va_arg(list, char *));
+			if (format[i] == 'p')
+				count += ft_puthex((unsigned long)va_arg(list, void *), 0);
+			if (format[i] == 'd')
+				count += ft_putnbr(va_arg(list, int));
+			if (format[i] == 'i')
+				count += ft_putnbr(va_arg(list, int));
+			if (format[i] == 'u')
+				count += ft_putnbr_unsigned(va_arg(list, unsigned int), 0);
+			if (format[i] == 'x')
+				count += ft_puthex(va_arg(list, unsigned int), 0);
+			if (format[i] == 'X')
+				count += ft_puthex(va_arg(list, unsigned int), 1);
+			if (format[i] == '%')
+				count += ft_putchar('%');
+		}
+		else
+			count += ft_putchar(format[i]);
+		i++;
+	}
 	va_end(list);
+	return (count);
 }
