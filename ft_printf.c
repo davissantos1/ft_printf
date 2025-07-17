530366d245fb09abd8ce110771b38988feb1917d
Davi Simoes Dos Santos <dasimoes@c1r8p4.42sp.org.br>
2025-07-16 23:04:12 -0300

    core of printf done

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,36 @@
 /*   By: dasimoes <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/14 12:59:00 by dasimoes          #+#    #+#             */
-/*   Updated: 2025/07/16 22:45:18 by dasimoes         ###   ########.fr       */
+/*   Updated: 2025/07/16 23:03:40 by dasimoes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+int	ft_printf_aux(const char letter, va_list list)
+{
+	int	count;
+
+	count = 0;
+	if (format[i] == 'c')
+		count += ft_putchar(va_arg(list, int));
+	if (format[i] == 's')
+		count += ft_putstr(va_arg(list, char *));
+	if (format[i] == 'p')
+		count += ft_puthex((unsigned long)va_arg(list, void *), 0);
+	if (format[i] == 'd')
+		count += ft_putnbr(va_arg(list, int));
+	if (format[i] == 'i')
+		count += ft_putnbr(va_arg(list, int));
+	if (format[i] == 'u')
+		count += ft_putnbr_unsigned(va_arg(list, unsigned int), 0);
+	if (format[i] == 'x')
+		count += ft_puthex(va_arg(list, unsigned int), 0);
+	if (format[i] == 'X')
+		count += ft_puthex(va_arg(list, unsigned int), 1);
+	if (format[i] == '%')
+		count += ft_putchar('%');
+}
 
 int	ft_printf(const char *format, ...)
 {
@@ -24,27 +49,7 @@ int	ft_printf(const char *format, ...)
 	while (format[i])
 	{
 		if (format[i] == '%')
-		{
-			i++;
-			if (format[i] == 'c')
-				count += ft_putchar(va_arg(list, int));
-			if (format[i] == 's')
-				count += ft_putstr(va_arg(list, char *));
-			if (format[i] == 'p')
-				count += ft_puthex((unsigned long)va_arg(list, void *), 0);
-			if (format[i] == 'd')
-				count += ft_putnbr(va_arg(list, int));
-			if (format[i] == 'i')
-				count += ft_putnbr(va_arg(list, int));
-			if (format[i] == 'u')
-				count += ft_putnbr_unsigned(va_arg(list, unsigned int), 0);
-			if (format[i] == 'x')
-				count += ft_puthex(va_arg(list, unsigned int), 0);
-			if (format[i] == 'X')
-				count += ft_puthex(va_arg(list, unsigned int), 1);
-			if (format[i] == '%')
-				count += ft_putchar('%');
-		}
+			count += ft_printf_aux(format[i], list);
 		else
 			count += ft_putchar(format[i]);
 		i++;
